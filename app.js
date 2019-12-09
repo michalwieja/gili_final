6078a8bee613c5300e7accd7e1209663e0719966
mwConceptX <mwieja@yahoo.com>
2019-12-09 20:05:43 +0100

    improvements

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
 setTimeout(function () {
     this.document.querySelector('.logo').classList.remove('banner')
 }, 2700);
+
+const navSlide = () => {
+    const menu = document.querySelector('.menu');
+    const nav = document.querySelector('.nav');
+    const links = document.querySelectorAll('.nav-link')
+
+    menu.addEventListener('click', () => {
+        nav.classList.toggle('navActive');
+        menu.classList.toggle('menuActive')
+        links.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ''
+            } else {
+                link.style.animation = `navLinkFade 1.5s ease forwards ${index / 5}s`;
+            }
+        })
+    })
+}
+
+navSlide();
